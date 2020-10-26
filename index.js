31dcb13404080f0c58118e9589cd816dff362973
Brandon Fonnemann <branjosef@gmail.com>
2020-10-25 21:18:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Code your solutions in this file
 
 function writeCards( namesArray, event ) {
-  let thankYouCards = []
+  let array = []
   for ( let i = 0; i < namesArray.length; i++ ) {
-    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    array.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
   }
-  return thankYouCards
-}
+  return array
+};
 
 
 function countDown(n) {
